bdf612a7a34d6501e1facaaf4b65b0f1ec856619
RyanKingWorkrise <ryan.king@workrise.com>
2025-10-02 12:45:59 +0100

    3100, 2941

--- daily/3100/waterbottles2.js
+++ daily/3100/waterbottles2.js
@@ -1,17 +1,13 @@
 "use strict";
 function maxBottlesDrunk(numBottles, numExchange) {
     let ogAmount = numBottles;
+    let operationcounter = 0;
     while (numExchange <= numBottles) {
         numBottles -= numExchange;
         numBottles++;
         numExchange++;
+        operationcounter++;
     }
-    console.log("numb" + numBottles);
-    console.log("og" + ogAmount);
-    //numExchange bottles of water then exchange them for a full bottle. Keep repeating this step until you cannot exchange bottles anymore.
-    return numBottles + ogAmount;
+    return operationcounter + ogAmount;
 }
 ;
-let aa = 10;
-let bb = 3;
-console.log(maxBottlesDrunk(aa, bb));
