5036738a4a929aa07c6ccdb448dc6ec3b9d7a71a
RyanKingWorkrise <ryan.king@workrise.com>
2025-10-02 12:46:28 +0100

    format

--- 2941/findchar.ts
+++ 2941/findchar.ts
@@ -1,19 +1,12 @@
 function findWordsContaining(words: string[], x: string): number[] {
   let indices = [];
-  for(let i = 0; i < words.length; i++){
-
-    for(let j = 0; j < words[i].length; j++){
-      console.log("i = " + i + "j = " + j + " incl check = " + indices.includes(i));
-    if(words[i][j] == x && !indices.includes(i)){
-      indices.push(i);
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words[i].length; j++) {
+      if (words[i][j] == x && !indices.includes(i)) {
+        indices.push(i);
+      }
     }
-    
-    }
-
   }
 
-
   return indices;
-};
-
-console.log(findWordsContaining(["leet","code"], "e"));
+}
