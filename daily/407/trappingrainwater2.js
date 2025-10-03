13df70622a4219842dcc3f9700dc46ad4a12c1e7
RyanKingWorkrise <ryan.king@workrise.com>
2025-10-03 10:15:05 +0100

    407

---
+++ daily/407/trappingrainwater2.js
@@ -0,0 +1,9 @@
+"use strict";
+function trapRainWater(heightMap) {
+    //heightmap m x n int matrix representing height of each unit cell in 2d elev map, return vol of water can trap
+    const m = heightMap.length;
+    const n = heightMap[0].length;
+    console.log(m + "" + n);
+    return 2;
+}
+;
